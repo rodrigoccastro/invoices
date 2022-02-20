dd49b1f052be211a6ec0276331a49bb0e8a81991
RodrigoCastro <rodrigo.castro@enjoei.com.br>
2022-02-19 21:58:06 -0300

    update messages to english

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,12 @@
 class UserController < ApplicationController
 
-  #gerar tokens
+  #fenerate tokens
   def token
 
-    #recebe email de param
+    #receive email by param
     email = params[:email]
     if !email.present?
-      #retorna msg de erro
+      #return error
       return_response(url_redirect: root_path+"?error_id=1", msg: "E-mail não enviado", status: :unprocessable_entity)
       return
     end
@@ -14,34 +14,34 @@ class UserController < ApplicationController
     #new object service to user
     user_service = UserService.new
 
-    #verifica a existencia do email na base
+    #verify existence of email in database
     user = user_service.find_user_by_email(email: email)
 
     if !user.present?
-      #inclui
+      # add
       user = user_service.new_user(email: email)
     else
 
-      #verifica se já existe token
+      # verify if has token
       if user_service.has_token?(user: user)
-        #recebe param que verifica se quer regerar
+        #recieve param for verify if regenerate token
         update = params[:update]
         if !update
-          #responde informando que já existe token
+          #return with message: token exist, do you want regenerate token?
           msg =  "Já existe token para este e-mail. Você deseja regerar o token?"
           return_response(url_redirect: root_path+"?error_id=2", msg: msg, status: :unprocessable_entity)
           return
         else
-          #caso contrario, é porque já recebeu esta info e deseja regerar
+          #other way, the user, before, recieve token and want regenerate
           user_service.generate_token(user: user)
         end
       else
-        #caso o token não exista, gera o token temporario
+        #if token not exist, generate token temp
         user_service.generate_token(user: user)
       end
     end
 
-    # responde: informe que o usuário precisará acessar o email dele.
+    # return message: the user need acess your email
     msg =  "Seu token foi regerado. Você precisará acessar o email para ativar o token e fazer login."
     return_response(url_redirect: root_path, msg: msg, status: :ok)
     return
@@ -81,9 +81,11 @@ class UserController < ApplicationController
       msg =  "O token foi ativado!"
       return_response(url_redirect: root_path, msg: msg, status: :ok)
     end
+
   end
 
   def login
+
     #recebe token de param
     token = params[:token]
     if !token
@@ -108,6 +110,7 @@ class UserController < ApplicationController
       msg =  "O login foi realizado corretamente!"
       return_response(url_redirect: "/invoice/list", msg: msg, status: :ok)
     end
+
   end
 
   def logout
