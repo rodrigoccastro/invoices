e448c5eeb3400db10bac55785e9b1c4412756a08
RodrigoCastro <rodrigo.castro@enjoei.com.br>
2022-02-20 23:49:32 -0300

    add send mail with pdf

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -1,18 +1,21 @@
+require "pdfkit"
+
 class InvoiceMailer < ApplicationMailer
 
   default from: 'notifications@example.com'
 
-  def send_mail_invoice(invoice:)
+  def send_mail_invoice
     #send invoice for list of emails
-    @url  = root_path + "/invoice/show?id=#{invoice.id}"
-    #attachment generate_pdf(invoice:)
+    @url  = root_path + "/invoice/show?id=#{@invoice.id}"
+    attachments["invoice-#{@invoice.id}.pdf"] = generate_pdf
     mail(to: invoice.emails, subject: "Invoice #{invoice.id}")
   end
 
   private
 
-  def generate_pdf(invoice:)
-    #...
-  end
+  def generate_pdf
+    PDFKit.new(root_path + "/invoice/show?id=#{@invoice.id}", :page_size => 'A3').to_file("invoice-#{@invoice.id}.pdf")
+    File.read("invoice-#{@invoice.id}.pdf")
+ end
 
 end
